aba050857898841260663fe002ab84aadc720248
simonzack <simonzack@email.com>
2014-01-21 01:13:29 +1100

    convert README.md to re-structured text for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
+import re
+import pypandoc
 from setuptools import setup, find_packages
+
+with open('README.md') as file:
+	long_description = pypandoc.convert(file.read(), 'rst', format='md')
+	#pandoc bug workaround
+	long_description = re.sub(r'(:alt:\s*(.+)\s*\r?\n)\s*\r?\n\s*\2', r'\1', long_description)
+	print(long_description)
 
 setup(
 	name='urxvt_tabbed',
 	version='0.1.0',
 	license='GPLv3',
 	description='Tab wrapper for urxt',
+	long_description=long_description,
 	url='https://github.com/simonzack/urxvt-tabbed',
 
 	author='simonzack',
@@ -18,7 +27,7 @@ setup(
 	scripts=['bin/urxvt-tabbed'],
 
 	classifiers = [
-		'Development Status :: 3 - Alpha',
+		'Development Status :: 4 - Beta',
 		'Intended Audience :: End Users/Desktop',
 		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 		'Programming Language :: Python :: 3',
